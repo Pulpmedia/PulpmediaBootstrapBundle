385b1545ac0b356e6b0abef68e35d6019d76bbb3
Alexandre Kilian <alexandre@diamonddev.com>
2013-08-23 13:42:03 +0200

    Initial Commit

---
+++ Controller/DefaultController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Pulpmedia\BootstrapBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/hello/{name}")
+     * @Template()
+     */
+    public function indexAction($name)
+    {
+        return array('name' => $name);
+    }
+	/**
+	 * @Template()
+	 */
+	public function baseAction(){
+		return array();
+	}
+	
+	/**
+	 * @Route("/pulpmedia/bootstrap/jumbotron")
+	 * @Template()
+	 */
+	public function jumbotronAction(){
+		return array();
+	}
+}
---
+++ PulpmediaBootstrapBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Pulpmedia\BootstrapBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class PulpmediaBootstrapBundle extends Bundle
+{
+}
